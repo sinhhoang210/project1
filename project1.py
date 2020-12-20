e85541ad9b4bd305082061d2fbfc7b84519af54d
SinhNguyen <sinhhoang210@gmail.com>
2020-12-20 15:35:19 +0700

    Issue 53 first version

--- project1.py
+++ project1.py
@@ -1,6 +1,13 @@
-#This is content of project 1
-def function1():
-    return 100
+# This is content of Issue 53
 
-def function2():
-    return 200
+def issue53():
+
+
+#This is content of project 1
+#def function1():
+#    return 100
+
+#def function2():
+#    return 200
+
+
